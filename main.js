80336da516a892fa542ac0d02d76b703e810e13d
Breyanna Vass <breyannavass@gmail.com>
2019-10-21 15:53:11 -0400

    adding DOM elements and variables

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+// DOM Elements
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
 // Generator Functions
 
 function getRandomLower() {
