140d6f4ebef5b8cfe279e472c924100894fe0699
Breyanna Vass <breyannavass@gmail.com>
2019-10-21 16:35:47 -0400

    added function to copy to clipboard

--- main.js
+++ main.js
@@ -31,6 +31,23 @@ generateEl.addEventListener("click", () => {
   );
 });
 
+// Copy password to clipboard
+clipboardEl.addEventListener("click", () => {
+  const textarea = document.createElement("textarea");
+  const password = resultEl.innerText;
+
+  if (!password) {
+    return;
+  }
+
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("Password copied to clipboard!");
+});
+
 // Generate Password Function
 function generatePassword(lower, upper, number, symbol, length) {
   let generatedPassword = "";
